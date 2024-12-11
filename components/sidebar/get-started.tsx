95cd1572eb3a0ee21d8c842c09b216cfebce9d97
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2024-12-11 17:21:10 +0000

    fix remove get started title

--- components/sidebar/get-started.tsx
+++ components/sidebar/get-started.tsx
@@ -1,41 +1,16 @@
 "use client";
 
 import {
-  BadgeCheck,
-  Bell,
-  ChevronsUpDown,
-  CreditCard,
-  LogOut,
-  Sparkles,
-} from "lucide-react";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from "@/components/ui/sidebar";
 import { CreateMessageDialog } from "../messages/create-message-dialog";
 
 export function NavGetStarted() {
-  const { isMobile } = useSidebar();
-
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Get Started</SidebarGroupLabel>
       <SidebarGroupContent>
         <SidebarMenu>
           <SidebarMenuItem>
