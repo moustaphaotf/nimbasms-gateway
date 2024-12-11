9a6cfd14a8b9f4ac3c36a34de3fab43e1760efc3
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2024-12-11 13:14:20 +0000

    Revert "fix(sender-form): fix name validation to allow space, dash and underscore"
    This reverts commit dcbbee77817bd8b81d296a7efe8c99a0ad9a39b1.

--- lib/schemas/sender.schema.ts
+++ lib/schemas/sender.schema.ts
@@ -4,9 +4,9 @@ export const createSenderSchema = z.object({
   name: z
     .string()
     .trim()
-    .min(2, "Le nom doit contenir au moins 3 caractères")
+    .min(3, "Le nom doit contenir au moins 3 caractères")
     .max(11, "Le nom ne peut pas dépasser 11 caractères")
-    .regex(/^[a-zA-Z0-9\s-]+$/, "Le nom doit contenir uniquement lettres, chiffres, - et _"),
+    .regex(/^[a-zA-Z0-9]+$/, "Le nom ne peut contenir que des lettres et des chiffres"),
 });
 
 export type CreateSenderFormData = z.infer<typeof createSenderSchema>;
